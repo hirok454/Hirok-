a8f52189f4036cf6331501daee4214a68e7abd5a
hirok454 <kalitaprahlad543@gmail.com>
2024-09-12 00:04:06 +0530

    Add files via upload

---
+++ firstprogram.py
@@ -0,0 +1,20 @@
+print("hello world")
+print("hirokjyoti kalita")
+print("hello world.","hirokjyoti kalita.")
+print(23)
+
+name="hirok"
+age=23
+print(name)
+##or
+print("My name is :",name)
+#how to identify data type
+print(type(name))
+print(type(age))
+#python is case sensitive
+a=2
+b=3
+print("sum",a+b)
+#or
+diff=a-b
+print("difference=",diff)
